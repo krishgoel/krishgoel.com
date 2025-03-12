a836475e7c342cf72fbc9fa858a3f59673438d37
KrishGoel <krishgoel3@gmail.com>
2025-03-12 14:29:51 +0530

    add: > be-me

---
+++ src/routes/be-me/+page.svelte
@@ -0,0 +1,55 @@
+<h1>> Be me</h1>
+<p>I figured no one would have the time to arrange everything I have done (<a href="/projects" aria-label="My Projects">projects</a>, <a href="https://www.linkedin.com/in/krishgoel" target="_blank" aria-label="My LinkedIn">internships + leadership</a>, <a href="https://scholar.google.com/citations?user=5a84VU4AAAAJ&hl=en" target="_blank" aria-label="My Research">research</a> and other bs) on a timeline to understand my profile, so here's a summary of my life + work hitherto.</p>
+<hr class="mt-8 mb-10" />
+<p>
+    > Be me, 0 (2003)<br/>
+    > 2009, get a computer, play FarmVille, get bored<br/>
+    > 2015, track runner + school football team captain <br/>
+    > 2017, work ass off to join one of the <a href="https://sanskritischool.edu.in/" target="_blank" aria-label="Sanskriti School">finest schools</a> in India<br/>
+    > 2018, 10th grade boards (92.2%)<br/>
+    > 2019, become VP of school's <a href="https://projectbeta.club" target="_blank" aria-label="ProjectBeta">CS + Entrepreneurship club</a>, lead teams and win hackathons + pitching events<br/>
+    > academic rebel, miss all PCM classes and JEE coaching to build 7 iterations of krishgoel.com and party with seniors<br/>
+    > drop out of Python at school, take up physical education to play football all day<br/>
+    > 2020, covid hit, school's online<br/>
+    > miss all online classes and learn RL<br/>
+    > launch a <a href="/projects/vkyd" aria-label="VKYD Fashion">clothing brand to battle fast fashion</a> with 2 friends, fail miserably<br/>
+    > no hopes of going to college<br/>
+    > build a <a href="https://eyena.krishgoel.com" target="_blank" aria-label="Eyena">smart mirror with temperature detection</a> for public spaces aftermath of the pandemic<br/>
+    > 2021, second wave hits<br/>
+    > build <a href="/projects/covaid" aria-label="CovAID">CovAID</a> with 5 friends - assemble a team of 40, build SOPs, train, deploy webapp, provide verified leads, save lives<br/>
+    > get acknowledged by <a href="https://www.instagram.com/covaid.resources/" target="_blank" aria-label="CovAID Instagram">celebrities</a> and <a href="/images/projects/covaid/covaid-hindustan-times.webp" target="_blank" aria-label="CovAID in Hindustan Times">newspaper media</a><br/>
+    > 12th grade boards (95.6%)<br/>
+    > somehow make it to <a href="https://www.manipal.edu/mit.html" target="_blank" aria-label="Manipal Institute of Technology">Manipal, Mangalore</a> to study mechantronics<br/>
+    > start liking finance, plan for MBA<br/>
+    > 2022, medical condition, need to be closer to home<br/>
+    > switch to <a href="https://jaipur.manipal.edu" target="_blank" aria-label="Manipal University Jaipur">Manipal, Jaipur</a> to study CS with AI & ML hons.<br/>
+    > 9.37 GPA<br/>
+    > 2023, realise MS > MBA<br/>
+    > change life trajectory<br/>
+    > realise I need research publications<br/>
+    > become a research intern at <a href="https://www.icmr.gov.in/" target="_blank" aria-label="Indian Council of Medical Research">ICMR</a> under <a href="https://scholar.google.co.in/citations?user=EE9ha9MAAAAJ&hl=en" target="_blank" aria-label="Dr. Hapreet Singh's Google Scholar Profile">Dr. Hapreet Singh</a>, for building a CTR acc. to WHO standards<br/>
+    > learn Django, ship code, get <a href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=5a84VU4AAAAJ&citation_for_view=5a84VU4AAAAJ:Y0pCki6q_DkC" target="_blank" aria-label="Design and Development of an Open-Source Clinical Trial Registry (CTR) Template Aligned with Who International Standards">first publication from desperation</a>, CTR platform distributed to 70+ attending countries at G20 under digital goods<br/>
+    > propelled to <a href="https://iisc.ac.in/" target="_blank" aria-label="Indian Institute of Science, Bangalore">IISc</a> to work on structring gold-standard medical imaging datasets at <a href="https://midas.iisc.ac.in/landing-page" target="_blank" aria-label="MIDAS Lab">Project MIDAS</a><br/>
+    > become president at <a href="https://www.instagram.com/randomizemuj/" target="_blank" aria-label="Randomize's Instagram">Randomize();</a> - my college's CSE Club<br/>
+    > meet cool people, write a lot of code, run the most successful marketing campaign in college history, onbaord 250+ paying members, teach student-filled auditoriums git, competitive programming and building neural nets from scratch<br/>
+    > intern at <a href="https://www.epson.co.in/industrial-robots" target="_blank" aria-label="Epson Robotics India">Epson Robotics India</a> - implement CV to a SCARA-L robot for a client, get bored<br/>
+    > 2024, organise the biggest student club college fest with 1600+ attendees. DJ at the fest, crowd loses contrl, suffer from success<br/>
+    > realise no good publications for MS apps + no one replying to 50+ research internship cold emails<br/>
+    > read <a href="https://arxiv.org/abs/2401.18059" target="_blank" aria-label="RAPTOR">RAPTOR</a>, figure out a retrieval optimisation<br/>
+    > write first actual research publication with <a href="https://www.linkedin.com/in/mahek-chandak" target="_blank" aria-label="Mahek Chandak's LinkedIn">Mahek Chandak</a> without any professor or supervision<br/>
+    > convince professors to miss classes, get GPU lab access, figure out LaTeX and writing a publication from scratch<br/>
+    > <a href="https://arxiv.org/abs/2406.09979" target="_blank" aria-label="HIRO">HIRO preprint</a> goes live, get <a href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=5a84VU4AAAAJ&citation_for_view=5a84VU4AAAAJ:zYLM7Y9cAGgC" target="_blank" aria-label="HIRO on Google Scholar">2 citations organically</a><br/>
+    > intern at <a href="https://www.machanirobotics.com" target="_blank" aria-label="Machani Robotics">Machani Robotics in blr</a>, to work <a href="https://x.com/krshgl/status/1896777057675432181" target="_blank" aria-label="">on AI and speech in humanoids</a><br/>
+    > build realtime speaker diarization + transcription, <a href="https://github.com/krishgoel/realtime-facial-profiling" target="_blank" aria-label="Realtime Facial Profiling">facial profiling</a>, multi-agent duplex conversation pipelines, and solve for behaviour, interruption-handling and proactivity have the best time ever<br/>
+    > take semester off from college (under the internship banner)<br/>
+    > start thinking about version control for context in conversations with LLMs<br/>
+    > start building an interface with version control for context, call it <a href="https://projectendgame.tech" target="_blank" aria-label="Project Endgame">Endgame</a><br/>
+    > pause development for college home-assignments<br/>
+    > try building a neural engine to check for "expiration of information" for proactivity in agents<br/>
+    > get 4 ambitious juniors to work with - feature engineering + exploring for 2 months, realise there is no ground-truth to compare against<br/>
+    > decide to build the world's first benchmark to instill a sense of time in machines, develop a paramteric representation of a continuous P(validity) over time<br/>
+    > 2025, opt out of placements and take another semester off from college to do something off my own<br/>
+    > <a href="https://x.com/krshgl/status/1884480430981931430" target="_blank" aria-label="Annotator Recruitment">recruit 12 annotators</a> to annotate the benchmark, synthetically generate samples, build the annotation platform, write guidelines, train annots, and build + train <a href="https://github.com/krishgoel/chronocept-baseline-models" target="_blank" aria-label="Chronocept Baseline Models">baseline models</a><br/>
+    > publish dataset to <a href="https://huggingface.co/datasets/krishgoel/chronocept" target="_blank" aria-label="Chronocept">HuggingFace</a> (preprint goes live soon)<br/>
+    > building <a href="https://projectendgame.tech" target="_blank" aria-label="Project Endgame">Endgame</a> full-time
+</p>
